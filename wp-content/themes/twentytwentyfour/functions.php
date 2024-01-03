35b280dde7c8b075efda7c659825b60a97d7963a
himanshu-verma-metacube <99269523+himanshu-verma-metacube@users.noreply.github.com>
2024-01-03 13:24:50 +0530

    Update functions.php

--- wp-content/themes/twentytwentyfour/functions.php
+++ wp-content/themes/twentytwentyfour/functions.php
@@ -11,8 +11,7 @@
 /**
  * Register block styles.
  */
-echo "github action poc";
- add_filter('use_block_editor_for_post', '__return_false', 10);
+add_filter('use_block_editor_for_post', '__return_false', 10);
 add_filter('use_widgets_block_editor', '__return_false', 100);
 
 add_theme_support('menus');
