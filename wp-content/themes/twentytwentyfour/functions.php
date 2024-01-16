d990dfc11afef2c9a4cab1631736e26b408a184b
trilokkumawatmag <trilok.kumawat@markanthony.com>
2024-01-16 16:09:57 +0530

    Added code on function.php

--- wp-content/themes/twentytwentyfour/functions.php
+++ wp-content/themes/twentytwentyfour/functions.php
@@ -12,6 +12,7 @@
  * Register block styles.
  */
 
+ echo "I am here"; die;
  add_filter('use_block_editor_for_post', '__return_false', 10);
 add_filter('use_widgets_block_editor', '__return_false', 100);
 
